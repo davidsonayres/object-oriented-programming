24aadd25518d1631d8caa3373861c49ef9007614
Michael <mjames.ayres@gmail.com>
2016-10-20 16:15:42 -0400

    Create deliver class method

--- paperboy.rb
+++ paperboy.rb
@@ -15,4 +15,14 @@ class Paperboy
       return 50 + (@experience / 2)
     end
   end
+
+  def deliver(start_address, end_address)
+    total_delivered = end_address - start_address
+    @experience += total_delivered
+    if total_delivered < quota
+      return (total_delivered * 0.25) - 2
+    elsif total_delivered >= quota
+      return (quota * 0.25) + (total_delivered - quota * 0.50)
+    end
+  end
 end
