af49f2c597da006da12728a47fd65640175d266d
Michael <mjames.ayres@gmail.com>
2016-10-20 17:19:50 -0400

    Create report class methos, update deliver class method

--- paperboy.rb
+++ paperboy.rb
@@ -5,7 +5,7 @@ class Paperboy
   def initialize(name, experience)
     @name       = name
     @experience = experience
-    @earnings   = earnings
+    @earnings   = 0
   end
 
   def quota
@@ -18,11 +18,23 @@ class Paperboy
 
   def deliver(start_address, end_address)
     total_delivered = end_address - start_address
-    @experience += total_delivered
     if total_delivered < quota
-      return (total_delivered * 0.25) - 2
+      money = (total_delivered * 0.25) - 2
     elsif total_delivered >= quota
-      return (quota * 0.25) + (total_delivered - quota * 0.50)
+      money = (quota * 0.25) + (total_delivered - quota) * 0.50
     end
+    @experience += total_delivered
+    @earnings = @earnings + money
+  end
+
+  def report
+    "I'm #{@name}! I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
   end
 end
+
+tommy = Paperboy.new("Tommy", 0)
+
+puts tommy.quota
+puts tommy.deliver(101, 160)
+puts tommy.earnings
+puts tommy.report
